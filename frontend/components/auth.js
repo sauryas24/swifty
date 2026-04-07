91557b47d73642297ae6b8c6cf16bb5cd1b397fe
vanshikagit24 <vanshikaagrawal868@gmail.com>
2026-04-07 16:00:23 +0530

    issues resolved

--- frontend/components/auth.js
+++ frontend/components/auth.js
@@ -1,9 +1,9 @@
 function protectPage(allowedRoles) {
-  const token = localStorage.getItem("token");
-  const role = localStorage.getItem("userRole");
+  const token = sessionStorage.getItem("token");
+  const role = sessionStorage.getItem("userRole");
 
   if (!token || !allowedRoles.includes(role)) {
-    localStorage.clear();
+    sessionStorage.clear();
     window.location.replace("Select_role_login.html");
   }
 }
