b99df9775abd7ec4fcd1744deac95c87ca7f05e7
Shruti Sahu <shruti132005@gmail.com>
2026-04-12 14:58:26 +0530

    clubs router added

--- backend/app/routers/clubs.py
+++ backend/app/routers/clubs.py
@@ -7,4 +7,6 @@ router = APIRouter(prefix="/api/clubs", tags=["Clubs"])
 
 @router.get("/", response_model=List[schemas.ClubResponse])
 def get_all_clubs(db: Session = Depends(database.get_db)):
-    return db.query(models.Club).all()
+    # Fetches all clubs from the database and uses ClubResponse to safely format as JSON
+    clubs = db.query(models.Club).all()
+    return clubs
